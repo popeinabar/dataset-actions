016e12250a1f47facfde081374ea84e7bfd7e9cd
Ayush <ayushphenomal1@gmail.com>
2024-04-19 21:28:03 +0530

    logged in kaggle using playwright

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const fs = require('fs');
 require("dotenv").config();
 const { parse } = require('csv-parse');
+const path = require('path');
+const fetchDataFromKaggle = require('./fetchdata.ts').fetchDataFromKaggle;
 
 const axios = require('axios');
 const { Sequelize } = require('sequelize');
@@ -11,9 +13,9 @@ const sequelize = new Sequelize('kaggle', 'root', 'root', {
     host: 'localhost',
     port:3333,
     dialect: 'mysql',
+    logging:false,
   })
   
-
 // Define the model for the names table
 const Name = sequelize.define('Name', {
   id: {
@@ -33,30 +35,57 @@ const Name = sequelize.define('Name', {
 });
 
 // Read and parse the CSV file
-const addFileToDb = () => {
-    let isFirstRow = true;//skipping the first row
-    fs.createReadStream('babyNamesUSYOB-full.csv')
-      .pipe(parse({ delimiter: ',' }))//splitting the values
-      .on('data', async (row) => {
-        if (isFirstRow) {
-          isFirstRow = false;
-          return; 
-        }
-        // Extract name and sex from each row
-        const [, name, sex,] = row;
-    
-        // Insert data into the database
-        try {
-          await Name.create({ name, sex });
-        } catch (error) {
-          console.error('Error inserting data:', error);
-        }
-      })
-      .on('end', () => {
-        console.log('CSV file successfully processed.');
-      });
-}
+const addFileToDb = async (extractionDir) => {
+  let isFirstRow = true;
+  let batchSize = 0;
+  let batchData = [];
 
+  await new Promise((resolve, reject) => {
+      fs.createReadStream(path.join(extractionDir, 'babyNamesUSYOB-full.csv'))
+          .pipe(parse({ delimiter: ',' }))
+          .on('data', async (row) => {
+              if (isFirstRow) {
+                  isFirstRow = false;
+                  return;
+              }
+
+              const [, name, sex] = row;
+              batchData.push({ name, sex });
+              batchSize++;
+
+              // If batch size reaches 500, insert data into the database
+              if (batchSize === 500) {
+                  try {
+                      await Name.bulkCreate(batchData);
+                      console.log('Batch inserted successfully.');
+                  } catch (error) {
+                      console.error('Error inserting data:', error);
+                      reject(error);
+                  }
+                  batchSize = 0;
+                  batchData = [];
+              }
+          })
+          .on('end', async () => {
+              // Insert remaining data if any
+              if (batchSize > 0) {
+                  try {
+                      await Name.bulkCreate(batchData);
+                      console.log('Final batch inserted successfully.');
+                  } catch (error) {
+                      console.error('Error inserting data:', error);
+                      reject(error);
+                  }
+              }
+              console.log('CSV file successfully processed.');
+              resolve();
+          })
+          .on('error', (error) => {
+              console.error('Error reading CSV file:', error);
+              reject(error);
+          });
+  });
+};
 
 
 // Function to send data to HubSpot API
@@ -65,7 +94,7 @@ async function sendDataToHubSpot() {
         const accessToken=`${process.env.TOKEN}`//token
       // Fetch data from MySQL database
       const names = await Name.findAll({
-        limit: 1,
+        limit: 10,
       });
         
       // Format data for HubSpot API
@@ -105,11 +134,17 @@ async function sendDataToHubSpot() {
   
   // Main function to execute both tasks
   async function main() {
-
-    // await addFileToDb()
-    await sendDataToHubSpot();
-
-  }
+    try {
+        const extractionDir = await fetchDataFromKaggle(); // Call fetchDataFromKaggle function
+        // const extractionDir = '/Users/ayushraj/Project/collage/interview/plena/kaggle hubspot/extracted'
+        console.log(extractionDir)
+        console.log("ectractiong of path completed")
+        await addFileToDb(extractionDir);
+        await sendDataToHubSpot();
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
   
   // Run the main function
   main();
